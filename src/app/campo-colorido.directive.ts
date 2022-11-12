e35b0833f536e20bc300b51e675f061dc2d2bebc
Danniel Oliveira <dannielgd@gmail.com>
2022-11-12 16:53:01 -0300

    Usando HostBinding e não mais Renderer

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,23 +1,22 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) {  }
+    @HostBinding('style.backgroundColor') corDeFundo: string = 'transparent';
 
   @HostListener('focus') aoGanharFoco() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-        'background-color', 'yellow');
+    this.corDeFundo = 'yellow'
+    // this.renderer.setStyle(this.elementRef.nativeElement,
+    //     'background-color', 'yellow');
   }
 
   @HostListener('blur') aoPerderFoco() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-        'background-color', 'transparent');
+    this.corDeFundo = 'transparent'
+    // this.renderer.setStyle(this.elementRef.nativeElement,
+    //     'background-color', 'transparent');
   }
 
 }
