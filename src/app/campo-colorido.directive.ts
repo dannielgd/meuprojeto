d10c457e464285a3206144996d73131afe3ed1b0
Danniel Oliveira <dannielgd@gmail.com>
2022-11-12 16:49:47 -0300

    Colocar campo colorido só quando receber foco

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
@@ -8,10 +8,16 @@ export class CampoColoridoDirective {
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
-  ) {
+  ) {  }
 
+  @HostListener('focus') aoGanharFoco() {
     this.renderer.setStyle(this.elementRef.nativeElement,
         'background-color', 'yellow');
+  }
+
+  @HostListener('blur') aoPerderFoco() {
+    this.renderer.setStyle(this.elementRef.nativeElement,
+        'background-color', 'transparent');
   }
 
 }
